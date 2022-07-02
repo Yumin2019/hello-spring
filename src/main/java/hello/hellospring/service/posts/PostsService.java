2f70794d66169bb4c26ff3c2be1e474b61c875c9
Yumin Kim <yumingames@gmail.com>
2022-07-03 00:37:28 +0900

    처음 ~ 110 page

---
+++ src/main/java/hello/hellospring/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package hello.hellospring.service.posts;
+
+import hello.hellospring.domain.posts.PostsRepository;
+import hello.hellospring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/hello/hellospring/web/PostApiController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.web;
+
+import hello.hellospring.service.posts.PostsService;
+import hello.hellospring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    // 여기부터
+}
